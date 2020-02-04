52292fa8d684359926cd5b5c2bf0563f0180ab44
Gordsky <60033211+Gordsky@users.noreply.github.com>
2020-02-04 10:33:35 +0800

    Update diy.js

--- js/diy.js
+++ js/diy.js
@@ -1,4 +1,3 @@
-
 function typedword() {
     $.ajax({
         type: "get",
@@ -21,54 +20,3 @@ function typedword() {
         }
     });
 }
-
-var OriginTitile = document.title,
-titleTime;
-document.addEventListener("visibilitychange",
-function() {
-    if (document.hidden) {
-        document.title = "你别走吖 Σ(っ °Д °;)っ";
-        clearTimeout(titleTime)
-    } else {
-        document.title = "你可算回来了 (｡•ˇ‸ˇ•｡) " + OriginTitile;
-        titleTime = setTimeout(function() {
-            document.title = OriginTitile
-        },
-        2000)
-    }
-});
-
-window.onload = function() {
-    var a = document.createElement("a");
-    a.href = document.referrer
-    var so = a.hostname;
-    var name = a.hostname.split(".")[1];
-    switch (name) {
-    case 'bing':
-        so = '必应搜索'
-        break;
-    case 'baidu':
-        so = '百度搜索'
-        break;
-    case 'so':
-        so = '360搜索'
-        break;
-    case 'google':
-        so = '谷歌搜索'
-        break;
-    case 'sm':
-        so = '神马搜索'
-        break;
-    case 'sogou':
-        so = '搜狗搜索'
-        break;
-    default:
-        so = a.hostname
-    }
-    var msg = '欢迎来自' + so + '朋友！';
-    $.message({
-        title: '来源提醒',
-        message: msg,
-        type: 'success'
-    })
-};
